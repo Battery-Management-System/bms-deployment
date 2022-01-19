f12bf55850caab3f2238de89e241a6724af5ffb2
thoverik <tho.pham@veriksystems.com>
2022-01-19 08:05:01 +0700

    update script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+ARG=$1
 VERSION=1.0.1
+if [[ -z "${ARG}" ]]; then
+	echo "Using default version ${VERSION} for deployment"
+else
+	VERSION=$ARG
+fi
 URL=https://github.com/nguyencatpham/bms-deployment/archive/refs/tags/v$VERSION.tar.gz
 BASE_DIR=/tmp/bms-deployment/bms-deployment-$VERSION
 
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo 'Install BMS Cloud v0.1.0'
+echo 'Install BMS Cloud'
+docker-compose pull
 docker-compose up -d --build
 echo 'BMS Cloud is started'
 echo 'Installation is completed. Navigate to http://0.0.0.0 to explore the BMS cloud UI'
