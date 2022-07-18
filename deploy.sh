f0559e8fca519bbc1b206d6b0a562983ee181bcb
thoverik <tho.pham@veriksystems.com>
2022-07-18 16:16:53 +0700

    fix: deployment can't use current docker name

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ARG=$1
-VERSION=1.0.4
+VERSION=1.1.5
 if [[ -z "${ARG}" ]]; then
 	echo "Using default version ${VERSION} for deployment"
 else
@@ -9,13 +9,13 @@ fi
 mkdir -p ./volume/mysql/data
 mkdir -p ./volume/timescale/data
 mkdir -p ./volume/uptime/data
+mkdir -p ./bms-deployment
 
 URL=https://github.com/nguyencatpham/bms-deployment/archive/refs/tags/v$VERSION.tar.gz
-BASE_DIR=/tmp/bms-deployment/bms-deployment-$VERSION
+BASE_DIR=./bms-deployment
 
 echo "URL: ${URL}"
 
-mkdir -p /tmp/bms-deployment
-curl -Ls $URL | tar xvz -C /tmp/bms-deployment/
+curl -Ls $URL | tar xvz -C $BASE_DIR
 
 cd $BASE_DIR && ./install.sh
